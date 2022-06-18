abc04fcb7c7ca73da0e0b835202f64729f3567b7
florianccj <florianccj@gmail.com>
2022-06-18 20:24:36 +0200

    feat(breaking change): add helper to list breaking changes
    RELEASE NOTE: List Breaking change
    
    A new module have been added to add the list off breaking change.
    It have been used in changelog as well as release note.
    So we can read now non retrocompatible evolution in those file.

--- auto-changelog/changelog/changelog.auto-changelog.setup.js
+++ auto-changelog/changelog/changelog.auto-changelog.setup.js
@@ -1,7 +1,5 @@
 const breakingListHelper = require('../hbs-helpers/breaking-list.helper');
-const releaseNoteFilterHelper = require('../hbs-helpers/release-note-filter.helper');
 
 module.exports = function (Handlebars) {
   Handlebars.registerHelper('breaking-list', breakingListHelper);
-  Handlebars.registerHelper('release-note-filter', releaseNoteFilterHelper);
 };
